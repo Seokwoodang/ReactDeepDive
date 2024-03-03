e4d9eabca9ecd44893970300bde00eff9b6006ef
Seokwoodang <swoo1429@naver.com>
2024-03-03 23:16:04 +0900

    20240303 스터디

--- src/function/first.js
+++ src/function/first.js
@@ -1,9 +1,6 @@
-const key = "a";
-const object = {
-  a: 1042,
-  b: 1,
-};
+const 이진수_2 = 0b10;
 
-const { [key]: hey } = object;
-
-console.log(hey);
+console.log(이진수_2 == (2).toString(10));
+console.log(0b10);
+console.log((2).toString(2));
+console.log((5).toString(2));
