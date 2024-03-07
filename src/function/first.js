59fad7b9a28c76bb41840bb76f183eff0cff2ed1
Seokwoodang <swoo1429@naver.com>
2024-03-08 00:34:39 +0900

    20240307 스터디

--- src/function/first.js
+++ src/function/first.js
@@ -1,6 +1,21 @@
-const 이진수_2 = 0b10;
+// const 이진수_2 = 0b10;
 
-console.log(이진수_2 == (2).toString(10));
-console.log(0b10);
-console.log((2).toString(2));
-console.log((5).toString(2));
+// console.log(이진수_2 == (2).toString(10));
+// console.log(0b10);
+// console.log((2).toString(2));
+// console.log((5).toString(2));
+
+function bar() {
+  console.log("bar");
+}
+function baz() {
+  console.log("baz");
+}
+// function foo() {
+//   console.log("foo");
+//   setTimeout(bar, 0); // setTimeout만 추가했다.
+//   baz();
+// }
+// foo();
+
+setTimeout(bar(), 0);
